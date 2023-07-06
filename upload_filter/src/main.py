3d5182d649354eb7d36a46f09e7a88334a9bb8d8
Jungseung Yang <xyloon@gmail.com>
2023-07-07 02:21:30 +0900

    Adding post method to fastAPI

--- upload_filter/src/main.py
+++ upload_filter/src/main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 @app.get("/{full_path:path}")
-async def root(request: Request, full_path: str):
-    return {"full_path": full_path, "query_params": request.query_params}
+async def get_root(request: Request, full_path: str):
+    return {"method": request.method, "full_path": full_path, "query_params": request.query_params}
+
+@app.post("/{full_path:path}")
+async def post_root(request: Request, full_path: str):
+    return {"method": request.method, "full_path": full_path, "json": await request.json()}
